31e45abba7d740457048062a912c971034e04f4e
josue943 <josue943@hotmail.com>
2025-04-23 21:06:32 -0600

    added chevrovup

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -1,62 +1,62 @@
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, ChevronUp } from "lucide-react";
 import { useState } from "react";
 
 const Cards = () => {
   const [openMenu, setOpenMenu] = useState<string[]>([]);
 
+  const checkMenu = (title: string) => openMenu.includes(title);
+
   const handleMenu = (title: string) => {
     setOpenMenu((prev) => {
-      if (prev.includes(title)) return prev.filter((item) => item !== title);
+      if (checkMenu(title)) return prev.filter((item) => item !== title);
       return [...prev, title];
     });
   };
 
   return (
-    <div className="custom-container  xl:py-5 py-2">
+    <div className="custom-container xl:py-5 py-2">
       <h1 className="text-center text-black sm:text-5xl text-3xl mb-8 md:mb-12">
         <span className="text-amber-600">Servicios</span>
       </h1>
-      <div className="grid grid-cols-[repeat(1,minmax(0,300px))] md:grid-cols-[repeat(2,minmax(0,300px))] lg:grid-cols-[repeat(3,minmax(100px,300px))] gap-y-8 gap-x-10 justify-center">
-        {services.map(({ title, items }) => (
-          <div
-            key={title}
-            className="text-black "
-            style={{ background: "#f7f7f7" }}
-          >
-            <div className="flex flex-col-reverse sm:flex-col">
-              <div
-                className={`bg-amber-900 w-full ${
-                  openMenu.includes(title) ? "block" : "hidden"
-                }  sm:block`}
-                style={{ height: 160 }}
-              ></div>
-              <div className="flex justify-between pb-2">
-                <h6 className="text-xl text-amber-600">{title}</h6>
-                <ChevronDown
-                  className="sm:hidden"
-                  onClick={() => handleMenu(title)}
-                />
+      <div className="grid grid-cols-[repeat(1,minmax(0,350px))] md:grid-cols-[repeat(2,minmax(0,300px))] lg:grid-cols-[repeat(3,minmax(100px,300px))] sm:gap-y-8 gap-y-4 gap-x-10 justify-center">
+        {services.map(({ title, items }) => {
+          const customClass = openMenu.includes(title) ? "block" : "hidden";
+          const Icon = openMenu.includes(title) ? ChevronUp : ChevronDown;
+          return (
+            <div
+              key={title}
+              className="text-black "
+              style={{ background: "#f7f7f7" }}
+            >
+              <div className="flex flex-col-reverse sm:flex-col">
+                <div
+                  className={`bg-amber-900 w-full ${customClass} sm:block`}
+                  style={{ height: 180 }}
+                ></div>
+                <div className="flex justify-between items-center py-1 px-2">
+                  <h6 className="text-xl text-amber-600">{title}</h6>
+                  <Icon
+                    className="sm:hidden"
+                    onClick={() => handleMenu(title)}
+                  />
+                </div>
+              </div>
+              <div className={`p-5 ${customClass} sm:block`}>
+                <ul>
+                  {items.map(({ id, description }) => (
+                    <li
+                      style={{ listStyle: "inside" }}
+                      className="text-md"
+                      key={id}
+                    >
+                      {description}
+                    </li>
+                  ))}
+                </ul>
               </div>
             </div>
-            <div
-              className={`p-5 ${
-                openMenu.includes(title) ? "block" : "hidden"
-              }  sm:block`}
-            >
-              <ul>
-                {items.map(({ id, description }) => (
-                  <li
-                    style={{ listStyle: "inside" }}
-                    className="text-md"
-                    key={id}
-                  >
-                    {description}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
