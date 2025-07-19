53b2340954fcabbecbd568c71895e11757ceabc0
Josue <josue943@hotmail.com>
2025-07-19 17:11:20 -0600

    improved seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,26 @@ import Cards from "@/components/cards";
 import Clients from "@/components/clients";
 import Mision from "@/components/mision";
 import Test from "@/components/test";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>
+          Constructora Civil MX | Construcción Profesional en México |
+          Constructora Monterrey
+        </title>
+        <meta
+          name="description"
+          content="Constructora Civil MX ofrece servicios de construcción de alta calidad en México. Especialistas en obra civil, residencial y comercial. Contáctanos hoy."
+        />
+        <meta
+          name="keywords"
+          content="constructora, construcción, México, obra civil, edificios, casas, constructoras en México"
+        />
+        <meta name="author" content="Editel" />
+      </Head>
       <Test />
       <Mision />
       <Cards />
